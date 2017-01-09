754d6f0ecd99bffab76329fd1db61f65f0ad24ef
Juboraj Sarker <juborajsarker001@gmail.com>
2017-01-09 20:56:36 +0600

    two activity done Main Activity+Registration Activity

---
+++ app/src/main/java/com/binary/blood/bloodbankmanagement/RegestrationActivity.java
@@ -0,0 +1,13 @@
+package com.binary.blood.bloodbankmanagement;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RegestrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regestration);
+    }
+}
